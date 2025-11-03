a282a7ea18289db40a34ae58cf8f4959b291c1e5
Jeyong Lee <jeyonglee@gmail.com>
2025-11-02 20:41:01 -0800

    fix config issue

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    cssPreload: false,
-  },
   // Configure static file serving
   async rewrites() {
     return [
